a5a4691f43d076aea8fd08749b672b99c40cbafc
emanuelly <159542297+imnotmanuu@users.noreply.github.com>
2024-09-16 09:48:41 -0300

    ook

---
+++ aula1.js
@@ -0,0 +1,13 @@
+const texto ="Ola mundo";
+const texto2 ="Ola mundo";
+const senha ="senhaSuperSegura456";
+const stringDeNumeros ="34567";
+
+const citacao = 'Meu nome é ';
+const meuNome ='Alex';
+
+console.log(citacao + meuNome)
+
+
+//concatenacqo-juntar duas ou mais variaveis (textos,operadoresa,variaveis)
+//template string ou template literal
