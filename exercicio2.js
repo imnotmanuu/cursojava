6fc91c557b0e28dca1acf731baffd1ec3bb3f2f7
emanuelly <159542297+imnotmanuu@users.noreply.github.com>
2024-09-18 08:50:47 -0300

    ok

---
+++ exercicio2.js
@@ -0,0 +1,25 @@
+// declaracao de varieaveis 
+let numero1 = 10;  // Primeiro numero 
+let numero2 = 5;  //segundo numero 
+
+//operadores arimeticas 
+let soma =numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+// Exibe os resultados no controle 
+console.log ("soma: " + soma);
+console.log ("subtracao " + subtracao);
+console.log ("multiplicacao " + multiplicacao);
+console.log ("divisao " +  divisao);
+
+//declaracao de variaveis
+let largura = 8;  // largura de retangulo 
+let altura =5; // altura do retangulo 
+
+//calculo da área
+let area = largura * altura;
+
+//exibe o resultado no console
+console.log("a area do retangulo e:" + area);
